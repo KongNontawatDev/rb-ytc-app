2154a3f5add4d67ba278e0c0a0a00506d27be39c
Kong Nontawat <kns.dev15885@gmail.com>
2025-01-26 12:52:05 +0700

    add vercel/analytics

--- src/App.tsx
+++ src/App.tsx
@@ -7,30 +7,32 @@ import { QueryClientProvider } from "@tanstack/react-query";
 import { queryClient } from "./libs/reactQuery";
 import { useEffect } from "react";
 import liff from "@line/liff";
-
+import { Analytics } from "@vercel/analytics/react";
 export default function App() {
+	useEffect(() => {
+		const initLiff = async () => {
+			try {
+				await liff.init({ liffId: import.meta.env.VITE_LIFF_ID });
+				if (!liff.isLoggedIn()) {
+					liff.login();
+				}
+			} catch (error) {
+				console.error("LIFF init failed", error);
+			}
+		};
+		initLiff();
+	}, []);
 
-  useEffect(() => {
-    const initLiff = async () => {
-      try {
-        await liff.init({ liffId: import.meta.env.VITE_LIFF_ID })
-        if (!liff.isLoggedIn()) {
-          liff.login()
-        }
-      } catch (error) {
-        console.error('LIFF init failed', error)
-      }
-    }
-    initLiff()
-  }, [])
-
-  return (
-    <I18nextProvider i18n={i18n}>
-      <QueryClientProvider client={queryClient}>
-        <AntDesignProvider>
-          <RouterProvider router={router} />
-        </AntDesignProvider>
-      </QueryClientProvider>
-    </I18nextProvider>
-  );
+	return (
+		<>
+			<Analytics />
+			<I18nextProvider i18n={i18n}>
+				<QueryClientProvider client={queryClient}>
+					<AntDesignProvider>
+						<RouterProvider router={router} />
+					</AntDesignProvider>
+				</QueryClientProvider>
+			</I18nextProvider>
+		</>
+	);
 }
